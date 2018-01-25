3c444a2d76b08a6aa4a49906d7c5dcc35873f4ab
Duanraudon <34836995+Duanraudon@users.noreply.github.com>
2018-01-25 20:37:04 +0800

    对Softmax Regression识别的进一步扩展
    添加隐含层，使用减轻过拟合的Dropout，自适应学习速率的Adagrad，以及解决梯度弥散的激活函数ReLU。

--- Multi-Layer Perceptron多层感知机.py
+++ Multi-Layer Perceptron多层感知机.py
@@ -1,51 +1,35 @@
-#%%
-# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-
-# Create the model
+#加载MNIST数据集，创建一个Interactive Session,这样以后执行无需指定Session。
 from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 sess = tf.InteractiveSession()
-
+#权重初始化为截断的正态分布，其标准差为0.1，
 in_units = 784
 h1_units = 300
 W1 = tf.Variable(tf.truncated_normal([in_units, h1_units], stddev=0.1))
 b1 = tf.Variable(tf.zeros([h1_units]))
 W2 = tf.Variable(tf.zeros([h1_units, 10]))
 b2 = tf.Variable(tf.zeros([10]))
-
+#定义x的占位符，Dropout的比率keep_prob即保留节点的概率，作为输入，定义成占位符。
 x = tf.placeholder(tf.float32, [None, in_units])
 keep_prob = tf.placeholder(tf.float32)
-
+#隐含层1，实现Dropout的功能，即随机将一部分节点设置为0，输出层y。
 hidden1 = tf.nn.relu(tf.matmul(x, W1) + b1)
 hidden1_drop = tf.nn.dropout(hidden1, keep_prob)
 y = tf.nn.softmax(tf.matmul(hidden1_drop, W2) + b2)
 
-# Define loss and optimizer
+#定义损失和优化器，y是预测的概率分布，y'是真实的概率分布，用来判断模型对真实概率分布估计的准确程度。cross_entropy一种损失函数，名为信息熵。
 y_ = tf.placeholder(tf.float32, [None, 10])
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 train_step = tf.train.AdagradOptimizer(0.3).minimize(cross_entropy)
 
-# Train
+#全局训练，batch数为100，一共为300000的样本，对全数据集进行了5轮迭代。
 tf.global_variables_initializer().run()
 for i in range(3000):
   batch_xs, batch_ys = mnist.train.next_batch(100)
   train_step.run({x: batch_xs, y_: batch_ys, keep_prob: 0.75})
 
-# Test trained model
+#测试训练的模型，寻找预测中最大的值，预测数字类别是否是正确的类别，再将布尔值转换为数值，求其平均正确训练值。再加保留节点数为1输出。
 correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 print(accuracy.eval({x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
