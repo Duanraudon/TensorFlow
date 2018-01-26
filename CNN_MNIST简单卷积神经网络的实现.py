87e3ae86bfafe01d63f43b24703ac50d03f65e4c
Duanraudon <34836995+Duanraudon@users.noreply.github.com>
2018-01-26 13:17:57 +0800

    对MNIST进行简单的卷积网络实现

--- CNN_MNIST简单卷积神经网络的实现.py
+++ CNN_MNIST简单卷积神经网络的实现.py
@@ -1,71 +1,57 @@
-#%%
-# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-
+#载入数据集
 from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 sess = tf.InteractiveSession()
 
-
+#对权重制造一些随机噪声打破完全对称，此处为截断的正态分布噪声，标准差设为0.1。
 def weight_variable(shape):
   initial = tf.truncated_normal(shape, stddev=0.1)
   return tf.Variable(initial)
-
+#对偏置增加一些正值为0.1。
 def bias_variable(shape):
   initial = tf.constant(0.1, shape=shape)
   return tf.Variable(initial)
-  
+#定义2维卷积函数，其中x为输入，w为参数，1*1的尺寸，1个通道，一个卷积数量。padding表示边界处理方式，这里表示让卷积输入和输出保持一致
 def conv2d(x, W):
   return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
-
+#定义2*2的最大池化函数
 def max_pool_2x2(x):
   return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
                         strides=[1, 2, 2, 1], padding='SAME')  
-                        
+#x是特征，y_是真实的标签。reshape是讲1D转化为2D，由原来的1*784转化为28*28。-1代表样本数量不固定，最后的1代表颜色通道数量。                        
 x = tf.placeholder(tf.float32, [None, 784])
 y_ = tf.placeholder(tf.float32, [None, 10])
 x_image = tf.reshape(x, [-1,28,28,1])
-                        
+#定义第一层卷积层                        
 W_conv1 = weight_variable([5, 5, 1, 32])
 b_conv1 = bias_variable([32])
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
-
+#定义第二层卷积层卷积数量变为64
 W_conv2 = weight_variable([5, 5, 32, 64])
 b_conv2 = bias_variable([64])
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2)
-
+#定义一个全连接层，隐含节点为1024，并将第二个卷积层的输出转化为1D
 W_fc1 = weight_variable([7 * 7 * 64, 1024])
 b_fc1 = bias_variable([1024])
 h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-
+#定义一个dropout层
 keep_prob = tf.placeholder(tf.float32)
 h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-
+#定义一个softmax层
 W_fc2 = weight_variable([1024, 10])
 b_fc2 = bias_variable([10])
 y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
-
+#定义损失函数
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-
+#定义评测准确性操作
 correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+#设置训练时比率为0.5，使用大小为50的batch，进行20000次迭代训练，参与训练样本为1000000。每一百次对准确性进行评测。
 tf.global_variables_initializer().run()
 for i in range(20000):
   batch = mnist.train.next_batch(50)
@@ -74,9 +60,6 @@ for i in range(20000):
         x:batch[0], y_: batch[1], keep_prob: 1.0})
     print("step %d, training accuracy %g"%(i, train_accuracy))
   train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
-
+#在测试集上进行全面测试。
 print("test accuracy %g"%accuracy.eval(feed_dict={
     x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
-
-
-
